4316df29d4399fe46eabc1e27f4cf8c3d497051f
ShivPrashant777 <shivprashant777@gmail.com>
2020-06-16 17:44:07 +0530

    Revamp Passport Authentication

--- server.js
+++ server.js
@@ -1,22 +1,37 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
+const cookiesession = require('cookie-session');
+const config = require('config');
+const passport = require('passport');
 
 // Body Parser
 app.use(express.json({ extended: false }));
+
+// Cookie Session
+app.use(
+	cookiesession({
+		maxAge: 24 * 60 * 60 * 1000,
+		keys: [config.get('cookieKey')],
+	})
+);
+
+// Initialize Passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Connect to Database
 connectDB();
 
 // Routes
 app.use('/api/teachers', require('./routes/teachers'));
-app.use('/api', require('./routes/auth'));
+app.use('/api/oauth', require('./routes/oauth'));
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello </h1>');
+	res.send('<h1>Hello </h1>');
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server Started On Port ${port}`);
+	console.log(`Server Started On Port ${port}`);
 });
